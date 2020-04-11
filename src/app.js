4d5ab979ca3f7794d20bfbe94e2698d2f265f889
Adeonir Kohl <adeonir@gmail.com>
2020-04-11 00:25:55 -0300

    Update prettier formating

--- src/app.js
+++ src/app.js
@@ -1,69 +1,69 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 
-const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const repositories = [];
-const likes = 0;
+const repositories = []
+const likes = 0
 
-app.get("/repositories", (request, response) => {
-  return response.json(repositories);
-});
+app.get('/repositories', (request, response) => {
+  return response.json(repositories)
+})
 
-app.post("/repositories", (request, response) => {
-  const { title, url, techs } = request.body;
-  const repository = { id: uuid(), title, url, techs, likes };
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body
+  const repository = { id: uuid(), title, url, techs, likes }
 
-  repositories.push(repository);
+  repositories.push(repository)
 
-  return response.json(repository);
-});
+  return response.json(repository)
+})
 
-app.put("/repositories/:id", (request, response) => {
-  const { id } = request.params;
-  const { title, url, techs } = request.body;
-  const repository = repositories.find((repository) => repository.id === id);
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params
+  const { title, url, techs } = request.body
+  const repository = repositories.find((repository) => repository.id === id)
 
   if (!repository) {
-    return response.status(400).json({ error: "Repository not found!" });
+    return response.status(400).json({ error: 'Repository not found!' })
   }
 
-  repository.title = title;
-  repository.url = url;
-  repository.techs = techs;
+  repository.title = title
+  repository.url = url
+  repository.techs = techs
 
-  return response.json(repository);
-});
+  return response.json(repository)
+})
 
-app.delete("/repositories/:id", (request, response) => {
-  const { id } = request.params;
-  const index = repositories.findIndex((repository) => repository.id === id);
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params
+  const index = repositories.findIndex((repository) => repository.id === id)
 
   if (index < 0) {
-    return response.status(400).json({ error: "Id not found!" });
+    return response.status(400).json({ error: 'Id not found!' })
   }
 
-  repositories.splice(index, 1);
+  repositories.splice(index, 1)
 
-  return response.status(204).send();
-});
+  return response.status(204).send()
+})
 
-app.post("/repositories/:id/like", (request, response) => {
-  const { id } = request.params;
-  const repository = repositories.find((repository) => repository.id === id);
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params
+  const repository = repositories.find((repository) => repository.id === id)
 
   if (!repository) {
-    return response.status(400).json({ error: "Repository not found!" });
+    return response.status(400).json({ error: 'Repository not found!' })
   }
 
-  repository.likes += 1;
+  repository.likes += 1
 
-  return response.json(repository);
-});
+  return response.json(repository)
+})
 
-module.exports = app;
+module.exports = app
